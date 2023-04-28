c1e1bfabe03322fbea1a15973e25a0a7f1c7e22e
sakshisharma78 <122162031+sakshisharma78@users.noreply.github.com>
2023-04-28 09:35:08 +0530

    guess the number
    we create a small game between user and computer that's game called guess the number in the game user guess the number which is given computer its a very

---
+++ guess the num.py
@@ -0,0 +1,77 @@
+while(True):
+    print('enter the level you want to play')
+    print('easy')
+    print('intermediant')
+    print('hard')
+    a=input('enter the level')
+    if a=='easy':
+        import random
+        rd=random.randrange(1,20)
+        n=input('enter your name')
+        while(True):
+            num=int(input("guess the num"))
+            if num>rd:
+                print("too large")
+            elif num<rd:
+                print("too small")
+            elif num==rd:
+                print("right")
+           
+                print(n,'you win the game')
+                break
+        else:
+             print("you loose the game!")
+        print('you want to play again')
+        w=input()
+        if w=='no':
+            break
+    elif a=='intermediate':
+        import random
+        c=10
+        rd=random.randrange(1,50)
+        n=input('enter your name')
+        while(c):
+            num=int(input("guess the num"))
+            if num>rd:
+                print("too large")
+            elif num<rd:
+                print("too small")
+            elif num==rd:
+                print("right")
+           
+                print(n,'you win the game')
+                break
+            c-=1
+        else:
+            print("you loose the game!")
+            print('you want to play again')
+            w=input()
+            if w=='no':
+               break
+         
+    elif a=='hard':
+        import random
+        c=4
+        rd=random.randrange(1,100)
+        n=input('enter your name')
+        while(c):
+            num=int(input("guess the num"))
+            if num>rd:
+                print("too large")
+            elif num<rd:
+                print("too small")
+            elif num==rd:
+                print("right")
+           
+                print(n,'you win the game')
+                break
+            c-=1
+        else:
+            print("you loose the game!")
+            print('you want to play again')
+            w=input()
+            if w=='no':
+               break
+          
+print('thank you,have a good day !')
+         
